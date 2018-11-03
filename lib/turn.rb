13d97f8b00042ca548f726a786ae8398ede285a3
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-03 16:52:56 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board,index)
+def valid_move?(board, index)
   if position_taken?(board, index) == false && board[index] = " "
     return true
   end
