de294463ca3bd66cbea8297d866280971e0dc901
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-03 17:39:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,5 +36,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
 input_to_index(user_input)
-  until user_input == true
+  until user_input.between(0,8)
+    turn(board)
 end
+
